b2ae29b4259428889cb0237c08f9eb7259495f0b
Yankun Song <yankun.l.song@gmail.com>
2023-04-21 07:27:45 -0400

    Update page.tsx

--- app/support/page.tsx
+++ app/support/page.tsx
@@ -39,7 +39,7 @@ export default function App() {
   };
 
   const A = 379.30;
-  const B = 38 * 7;
+  const B = (26.75 + 15 + 30） * 7;
   return (
     <div className="container">
       <div className="text-container">
