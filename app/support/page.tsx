25e7d29222367c35205b1e667332719d11ff8018
unknown <yankun.l.song@gmail.com>
2023-09-17 10:21:14 -0400

    oct

--- app/support/page.tsx
+++ app/support/page.tsx
@@ -103,7 +103,7 @@ export default function App() {
         });
         setShowText(true);
         console.log("isAdvanced", isAdvanced);
-        const notice = isAdvancedTmp ? "高级密码是：aug44" : "普通密码是：aug";
+        const notice = "密码是：oct";
         alert(notice);
         break;
       }
@@ -121,38 +121,24 @@ export default function App() {
         {/* <p>欢迎大家使用我的网站!</p>
         <p>到月末了，请大家对6月的使用情况，和对自己的帮助大小自助付费。</p>
         <p>多用多付，少用少付，不用不付。 </p> */}
-        <p>上个月网站开放了GPT4的接口，也随之产生了大量的费用。</p>
         <p>
-          为了兼顾大家的普通需求，和对GPT4的需求，我决定暂时同时采用两种模式。
+          <strong>请大家对8月和9月至今的使用情况自行付费。</strong>
         </p>
         <p>
-          1.
-          普通模式：对所有有过付费的用户开放，并且先用后付。每个月底，自愿对这个月的使用情况付费。多用多付，少用少付，不用不付。
+          8月试着开放了GPT4接口，收费30元。但是官方价格过高，这个收费仅够支付10天不到的费用。如果要维持平衡，得收费100多元一月。
         </p>
-        <p>比如这次付费，就是对7月的使用情况付费。</p>
+        <p>所以目前网站将只开放GPT3.5。</p>
+        <p>感谢有一些朋友在8月底9月初自行回到了这个页面进行了支持。</p>
         <p>
-          2.
-          高级模式：每次付费30元及以上，即可获得有GPT4权限的密码。该密码有效期为一个月。仅供自己使用，不得分享。
-        </p>
-        <p>
-          下面的表单会自动判别数额，大于30即显示高级模式的密码。请如实填写。如有造假，永久拉黑。
-        </p>
-        <p>
-          为了方便我核对，请大家付款的数额带上随意的小数点，如支付30.17，
-          40.46等。
-        </p>
-        <p>
-          模型可以在网页左下角的设置中选择，gpt-4或者gpt-3.5-turbo。gpt4仅在使用的是有权限的密码时生效。
-        </p>
-        <p>
-          判断自己用的是哪个模型，可以问GPT，“我为什么没有参加我爸爸妈妈的婚礼？”GPT4会说“因为您的父母在您出生之前就已经结婚了”，而3.5则意识不到这一点。
+          <strong>
+            8月底有两位朋友8月底支付了GPT4的价格，请务必与我联系，我将给你们退款。
+          </strong>
         </p>
         <br />
         <p>关于付费：</p>
         <p>
           本网站初衷是方便董董星球的朋友体验GPT，无盈利目的。放收款码是因为GPT的访问不是免费的，通过API对话会产生一定的费用。
         </p>
-        <p>官方GPT4的单价是GPT3.5的20倍。</p>
         <p>
           目前只有初期主动捐赠过的用户才在白名单里。所以如果你是刚知道这个网站并且想使用，请邮件与我说明情况。
         </p>
@@ -174,8 +160,8 @@ export default function App() {
         <Image src="wechat.png" alt="wechat" className="image" />
       </div> */}
       <div className="hidden-text-container">
-        {showText && isAdvanced && <p>高级密码: aug44</p>}
-        {showText && !isAdvanced && <p>普通密码: aug</p>}
+        {showText && isAdvanced && <p>密码: oct</p>}
+        {showText && !isAdvanced && <p>密码: oct</p>}
       </div>
       <div className="image-container">
         <p>支付宝二维码：</p>
